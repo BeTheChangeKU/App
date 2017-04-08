816a26e6096b728511349b86e3c0fb43a4eb3628
Jeet Patel <jeet.patel1022@gmail.com>
2017-04-07 22:52:38 -0400

    Added barebones iOS Project

---
+++ BeTheChange/BeTheChange/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeTheChange
+//
+//  Created by Jeet Patel on 4/7/17.
+//  Copyright © 2017 BeTheChange. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
